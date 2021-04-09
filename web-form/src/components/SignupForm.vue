049cd3eaec90b08088c65dcc13b5adb5d8fb9b3f
Bricx Carasco <bricxraincarasco21@gmail.com>
2021-04-10 00:16:42 +0800

    Handle form submit and do password validation

--- web-form/src/components/SignupForm.vue
+++ web-form/src/components/SignupForm.vue
@@ -1,10 +1,11 @@
 <template>
-	<form>
+	<form @submit.prevent="handleSubmit">
 		<label>Email</label>
 		<input v-model="email" type="email" required>
 
 		<label>Password</label>
 		<input v-model="password" type="password" required>
+		<div v-if="passwordError" class="error">{{ passwordError }}</div>
 
 		<label>Role</label>
 		<select v-model="role">
@@ -23,6 +24,10 @@
 			<input type="checkbox" v-model="terms" required>
 			<label>Accept terms and conditions</label>
 		</div>
+
+		<div class="submit">
+			<button>Create an account</button>
+		</div>
 	</form>
 </template>
 
@@ -35,7 +40,8 @@ export default {
 			role: 'developer',
 			terms: false,
 			tempSkill: '',
-			skills: []
+			skills: [],
+			passwordError: ''
 		}
 	},
 	methods: {
@@ -51,6 +57,17 @@ export default {
 			this.skills = this.skills.filter((item) => {
 				return item !== skill
 			})
+		},
+		handleSubmit() {
+			this.passwordError = this.password.length > 5 ? '' : 'Password must be at least 6 characters long'
+
+			if (!this.passwordError) {
+				console.log('email: ', this.email)
+				console.log('password: ', this.password)
+				console.log('role: ', this.role)
+				console.log('skills: ', this.skills)
+				console.log('terms accepted: ', this.terms)
+			}
 		}
 	}
 }
@@ -101,5 +118,23 @@ export default {
 		font-weight: bold;
 		color: #777;
 		cursor: pointer;
+	}
+	button {
+		background: #0b6dff;
+		border: 0;
+		padding: 10px 20px;
+		margin-top: 20px;
+		color: white;
+		border-radius: 20px;
+		cursor: pointer;
+	}
+	.submit {
+		text-align: center;
+	}
+	.error {
+		color: #ff0062;
+		margin-top: 10px;
+		font-size: 0.8em;
+		font-weight: bold;
 	}
 </style>

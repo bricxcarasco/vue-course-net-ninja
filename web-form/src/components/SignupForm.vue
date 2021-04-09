554d1af5c5c96780ae6dab207fc68cb921d5f686
Bricx Carasco <bricxraincarasco21@gmail.com>
2021-04-10 00:02:29 +0800

    Implement delete skill per clickuse filter arr func

--- web-form/src/components/SignupForm.vue
+++ web-form/src/components/SignupForm.vue
@@ -16,7 +16,7 @@
 		<input type="text" v-model="tempSkill" @keyup.ctrl="addSkill">
 
 		<div v-for="skill in skills" :key="skill" class="pill">
-			{{ skill }}
+			<span @click="deleteSkill(skill)">{{ skill }}</span>
 		</div>
 
 		<div class="terms">
@@ -46,6 +46,11 @@ export default {
 				}
 				this.tempSkill = ''
 			}
+		},
+		deleteSkill(skill) {
+			this.skills = this.skills.filter((item) => {
+				return item !== skill
+			})
 		}
 	}
 }
@@ -84,5 +89,17 @@ export default {
 		margin: 0 10px 0 0;
 		position: relative;
 		top: 2px;
+	}
+	.pill {
+		display: inline-block;
+		margin: 20px 10px 0 0 ;
+		padding: 6px 12px;
+		background: #eee;
+		border-radius: 20px;
+		font-size: 12px;
+		letter-spacing: 1px;
+		font-weight: bold;
+		color: #777;
+		cursor: pointer;
 	}
 </style>

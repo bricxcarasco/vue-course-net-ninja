640424223851bc91e74913fbee8b58cf25c966a0
Bricx Carasco <bricxraincarasco21@gmail.com>
2021-04-09 21:39:04 +0800

    Implement ranking and do some styling

--- reaction-timer/src/components/Results.vue
+++ reaction-timer/src/components/Results.vue
@@ -1,15 +1,33 @@
 <template>
   <div>
       <p>Your reaction time is {{ score }}ms</p>
+      <h1 class="rank">{{ rank }}</h1>
   </div>
 </template>
 
 <script>
 export default {
-    props: ['score']
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = 'Ninja Fingers'
+        } else if (this.rank < 400) {
+            this.rank = 'Rapid Reflexes'
+        } else {
+            this.rank = 'Snail pace...'
+        }
+    }
 }
 </script>
 
 <style>
-
+    .rank {
+        color: #0faf87;
+        font-weight: bold;
+    }
 </style>

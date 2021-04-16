ce2365f285ff8a765c40e95988348b34fe6f48a5
Bricx Carasco <bricxraincarasco21@gmail.com>
2021-04-16 23:32:45 +0800

    Programatically use back, forward and redirect of vue router

--- ninja-jobs/src/App.vue
+++ ninja-jobs/src/App.vue
@@ -4,8 +4,29 @@
     <router-link :to="{ name: 'About' }">About</router-link> |
     <router-link :to="{ name: 'Jobs' }">Jobs</router-link>
   </div>
+
+  <button @click="redirect">Redirect</button>
+  <button @click="goBack">Go Back</button>
+  <button @click="goForward">Go Forward</button>
+
   <router-view/>
 </template>
+
+<script>
+export default {
+  methods: {
+    redirect() {
+      this.$router.push({ name: 'Home' })
+    },
+    goBack() {
+      this.$router.go(-1)
+    },
+    goForward() {
+      this.$router.go(1)
+    }
+  }
+}
+</script>
 
 <style>
 #app {
@@ -31,5 +52,12 @@
 #nav a.router-link-exact-active {
   color: white;
   background: crimson;
+}
+
+button {
+  margin: 0 10px;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
 }
 </style>

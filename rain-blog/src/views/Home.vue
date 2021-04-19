469f5a57b0d167639123737f6f58406f32ac0884
Bricx Carasco <bricxraincarasco21@gmail.com>
2021-04-19 22:36:59 +0800

    Use ref and reactive in composition api

--- rain-blog/src/views/Home.vue
+++ rain-blog/src/views/Home.vue
@@ -1,23 +1,33 @@
 <template>
   <div class="home">
-    <h1>My name is {{ name }} and age is {{ age }}</h1>
-    <button @click="handleClick">Handle Click</button>
+    <h1>Home</h1>
+    <h2>Refs</h2>
+    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
+    <button @click="updateNinjaOne">Update ninja one</button>
+    <h2>Reactive</h2>
+    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
+    <button @click="updateNinjaTwo">Update ninja two</button>
   </div>
 </template>
 
 <script>
+import { ref, reactive } from '@vue/reactivity'
 
 export default {
   name: 'Home',
   setup() {
-    let name = 'Mario'
-    let age = 30
+    const ninjaOne = ref({ name: 'mario', age: 30 })
+    const ninjaTwo = reactive({ name: 'luigi', age: 35 })
 
-    const handleClick = () => {
-      console.log('you clicked me!')
+    const updateNinjaOne = () => {
+      ninjaOne.value.age = 40
     }
 
-    return { name, age, handleClick }
+    const updateNinjaTwo = () => {
+      ninjaTwo.age = 45
+    }
+
+    return { ninjaOne, ninjaTwo, updateNinjaOne, updateNinjaTwo }
   }
 }
 </script>

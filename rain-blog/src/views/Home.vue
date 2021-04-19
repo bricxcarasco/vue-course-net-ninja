0dd3d7072ecc33073b0c251f5df285658e5c8bd0
Bricx Carasco <bricxraincarasco21@gmail.com>
2021-04-19 22:15:35 +0800

    Use vue setup function

--- rain-blog/src/views/Home.vue
+++ rain-blog/src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>My name is {{ name }} and age is {{ age }}</h1>
+    <button @click="handleClick">Handle Click</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  setup() {
+    let name = 'Mario'
+    let age = 30
+
+    const handleClick = () => {
+      console.log('you clicked me!')
+    }
+
+    return { name, age, handleClick }
   }
 }
 </script>
